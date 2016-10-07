eb273d6a9b7edf9e9789b56af41761a9a5f8fdac
Pesegato <pesegato@outlook.com>
2016-10-07 15:17:48 +0200

    WIP

--- src/main/java/batched/TestBatchedAppState.java
+++ src/main/java/batched/TestBatchedAppState.java
@@ -3,27 +3,21 @@ package batched;
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.BaseAppState;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
-import com.jme3.scene.VertexBuffer;
-import com.jme3.scene.shape.Quad;
 import com.jme3.util.BufferUtils;
 import com.pesegato.MonkeySheet.MSContainer;
 import com.pesegato.MonkeySheet.MSControl;
 import com.pesegato.MonkeySheet.MSMaterialControl;
 import com.pesegato.MonkeySheet.MonkeySheetAppState;
-import com.pesegato.MonkeySheet.actions.MSAction;
 import com.pesegato.MonkeySheet.quad.SpriteQuad;
 import com.pesegato.goldmonkey.GM;
 
 import java.nio.FloatBuffer;
 
 import static com.jme3.scene.VertexBuffer.Type.*;
-import static com.pesegato.MonkeySheet.MSGlobals.SPRITE_SIZE;
 
 
 public class TestBatchedAppState extends BaseAppState {
@@ -31,7 +25,7 @@ public class TestBatchedAppState extends BaseAppState {
     float tTPF = 0;
     boolean idling = false;
 
-    public static int SIZE = 1;
+    public static int SIZE = 4;
 
     float cc[] = new float[16];
     FloatBuffer posBuffer, msPosBuffer;
@@ -47,16 +41,15 @@ public class TestBatchedAppState extends BaseAppState {
         quads = new SpriteQuad[SIZE];
         Vector2f[] texCoord = new Vector2f[4 * SIZE];
         int[] indexes = new int[6 * SIZE];
-        float[] msPos = new float[SIZE];
 
         mesh.setBuffer(Position, 3, BufferUtils.createFloatBuffer(new Vector3f[4 * SIZE]));
-        mesh.setBuffer(TexCoord2, 1, BufferUtils.createFloatBuffer(msPos));
+        mesh.setBuffer(TexCoord2, 1, BufferUtils.createFloatBuffer(new float[4 * SIZE]));
         posBuffer = (FloatBuffer) mesh.getBuffer(Position).getData();
         msPosBuffer = (FloatBuffer) mesh.getBuffer(TexCoord2).getData();
         for (int i = 0; i < SIZE; i++) {
             int j = i / 500;
             int k = i - j * 500;
-            quads[i]=new SpriteQuad(i, posBuffer, texCoord,indexes, msPosBuffer, msPos);
+            quads[i]=new SpriteQuad(i, posBuffer, texCoord,indexes, msPosBuffer);
             quads[i].setPosition(k , j);
 
         }
@@ -75,7 +68,7 @@ public class TestBatchedAppState extends BaseAppState {
         //Geometry geo = MSAction.createGeometry("spatial", 1f, 1f);
 
         Geometry geo = new Geometry("monkey", mesh);
-        geo.setLocalScale(128);
+        geo.setLocalScale(64);
 
 
         msc = new MSControl("run");
@@ -88,6 +81,9 @@ public class TestBatchedAppState extends BaseAppState {
     }
 
     public void update(float tpf) {
+        for (int i=0;i<SIZE;i++) {
+            quads[i].setSFrame(i);
+        }
     }
 
     @Override
